fdbc84a27fad0cccfde85bc5077e07533d5f289b
Lew L3V666 Podmoskovnov <lev.tmb.l29@gmail.com>
2025-12-08 21:14:22 +0300

    + rest_plus

--- blood/30-50clean.py
+++ blood/30-50clean.py
@@ -4,6 +4,17 @@ import numpy as np
 
 data = np.array(pd.read_csv('/home/l3v/repo/Pelevin-Podmoskovnov-Lukin/blood/30-50clean.csv')[1:])
 
+data[:, 1] *= 80.65
+
 plt.plot(data[:, 0], data[:, 1])
+
+plt.ylabel('$\Delta P$, мм рт. ст.')
+plt.xlabel('$t$, с')
+
+plt.grid(True, which='major', linestyle='-', linewidth=0.5)
+plt.grid(True, which='minor', linestyle='-', linewidth=0.3)
+
+plt.minorticks_on()
+
 
 plt.show()
